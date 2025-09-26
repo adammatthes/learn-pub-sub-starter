0ed95e6264524e93df6ff2ebb7af5491573bdb8b
adammatthes <adammatthes@gmail.com>
2025-09-25 22:33:15 -0400

    Set up connection to MQ in server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
 
+)
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+
+	myConnection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		fmt.Println("MQ connection failed:", err)
+		return
+	}
+
+	defer myConnection.Close()
+
+	fmt.Println("MQ Connection successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("\nProgram shutting down...")
 }
